995e7d2a9c63c16246ebb8e5c226019f127bfa39
Elle <evargas@avvo.com>
2018-03-28 13:38:03 -0700

    Initial commit

---
+++ lib/snapeth.ex
@@ -0,0 +1,18 @@
+defmodule Snapeth do
+  @moduledoc """
+  Documentation for Snapeth.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Snapeth.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/snapeth_test.exs
@@ -0,0 +1,8 @@
+defmodule SnapethTest do
+  use ExUnit.Case
+  doctest Snapeth
+
+  test "greets the world" do
+    assert Snapeth.hello() == :world
+  end
+end
