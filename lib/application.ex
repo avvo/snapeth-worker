7f9678b94d93747f35eaa2b9d5dcf71f72b89c08
Elle <evargas@avvo.com>
2018-03-28 13:51:10 -0700

    Got Distillery working

---
+++ lib/application.ex
@@ -0,0 +1,6 @@
+defmodule Snapeth.Application do
+  use Application
+  def start(_type, _args) do
+    IO.puts "hello application"
+  end
+end
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Snapeth.MixProject do
+defmodule Worker.MixProject do
   use Mix.Project
 
   def project do
@@ -7,22 +7,30 @@ defmodule Snapeth.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx],
+      mod: {Snapeth.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp aliases do
+    [
+      test: "test --no-start"
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:distillery, "~> 1.5"},
+      {:poison, "~> 3.1"},
+      {:slack, "~> 0.13"},
+      {:mox, "~> 0.3", only: :test}
     ]
   end
 end
