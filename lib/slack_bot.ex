8b5e8d105abacf133f63a5e252e943b68d979296
Elle <evargas@avvo.com>
2018-04-06 17:39:22 -0700

    Change copy for help message

--- lib/slack_bot.ex
+++ lib/slack_bot.ex
@@ -32,7 +32,7 @@ defmodule Snapeth.SlackBot do
   end
 
   def help(message, slack, state) do
-    send_message("Hi! To give snaps, start by tagging a team member and we'll instruct you from there. For example: @slackbot", message.channel, slack)
+    send_message("Have you seen someone demonstrate a positive and inclusive behavior at work? Tag that teammate here to give them an appreciative snap!\nFor example: @slackbot", message.channel, slack)
     state
   end
 
